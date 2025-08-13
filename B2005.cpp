34be2e12e8c7b7a188ccde960a12abf53d3d9063
xenon-xys <hzyqy331@163.com>
2025-08-13 20:29:56 +0800

    B2005 AC

---
+++ B2005.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char c;
+    std::cin>>c;
+    std::cout<<"  "<<c<<std::endl;
+    std::cout<<" "<<c<<c<<c<<std::endl;
+    std::cout<<c<<c<<c<<c<<c<<std::endl;
+    return 0;
+}
+//https://www.luogu.com.cn/problem/B2005
