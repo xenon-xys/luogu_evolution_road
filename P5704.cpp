5c35729c9df21f8d7cd34a680f56ca500ca5c9fd
xenon-xys <hzyqy331@163.com>
2025-08-13 20:36:30 +0800

    P5704 AC

---
+++ P5704.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char c;
+    std::cin>>c;
+    std::cout<<char(c-32)<<std::endl;
+    return 0;
+}
+//https://www.luogu.com.cn/problem/P5704
