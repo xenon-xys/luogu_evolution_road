524921c88b058de392c8544a226aabd833090fd3
xenon-xys <hzyqy331@163.com>
2025-08-13 20:26:46 +0800

    P1001 AC

---
+++ P1001.cpp
@@ -0,0 +1,10 @@
+/*任何一个伟大的思想，都有一个微不足道的开始*/
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    std::cin>>a>>b;
+    std::cout<<a+b<<std::endl;
+    return 0;
+}
+//https://www.luogu.com.cn/problem/P1001
