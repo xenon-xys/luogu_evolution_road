a21ee0557604d3d951645b5b58fb561a9c56f803
xenon-xys <hzyqy331@163.com>
2025-08-16 21:10:20 +0800

    P3954 AC

---
+++ P3954.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    int ans=a*0.2+b*0.3+c*0.5;
+    cout << ans << endl;
+    return 0;
+}
+//https://www.luogu.com.cn/problem/P1421
