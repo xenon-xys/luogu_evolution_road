820a765974d18bec65df4d3b2ff2b8ebcf824e0b
xenon-xys <hzyqy331@163.com>
2025-08-16 21:07:17 +0800

    P1421 AC

---
+++ P1421.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin >> a >> b;
+    int n=a*10+b;
+    int ans=n/19;
+    cout << ans << endl;
+    return 0;
+}
+//https://www.luogu.com.cn/problem/P1421
