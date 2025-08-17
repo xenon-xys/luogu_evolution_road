852682cc7e62fc1697f72cb363209f07f2fbd9ac
xenon-xys <hzyqy331@163.com>
2025-08-17 23:21:17 +0800

    P5713 AC

---
+++ P5713.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(5*n<3*n+11) cout<<"Local"<<endl;
+    else cout<<"Luogu"<<endl;
+    return 0;
+}
+//https://www.luogu.com.cn/problem/P5713
