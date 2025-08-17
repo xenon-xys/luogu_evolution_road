3c2013e8c08526de68b9bd5cda50a71fd3bb964b
xenon-xys <hzyqy331@163.com>
2025-08-17 23:19:04 +0800

    P5712 AC

---
+++ P5712.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    if(x==1||x==0) cout<<"Today, I ate "<<x<<" apple."<<endl;
+    else cout<<"Today, I ate "<<x<<" apples."<<endl;
+    return 0;
+}
+//https://www.luogu.com.cn/problem/P5712
